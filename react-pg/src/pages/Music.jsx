f7cfe4557bdad352b0dd0f233cb1a3a534e2a706
pascualgazquez <pascual.gazquez.compan@gmail.com>
2025-03-04 16:27:38 +0100

    music player h

--- react-pg/src/pages/Music.jsx
+++ react-pg/src/pages/Music.jsx
@@ -3,6 +3,7 @@ import "./Music.css"
 import Cover from  "../Components/Cover/Cover";
 
 import MusicPlayer from "../Components/MusicPlayer/MusicPlayer";
+import MusicPlayerH from "../Components/MusicPlayerH/MusicPlayerH";
 import music from '../assets/titles/music.gif'
 import musicCover from '../assets/more/music.gif'
 
@@ -16,8 +17,10 @@ const Music = () => {
     return (
       <>
         <Cover color = "#fabebe" title={music} picture={musicCover}/>
+
         <h1>CHARACTERS</h1>
         <p>These tracks are made to </p>
+
         <div className = "music-section">
           <MusicPlayer 
             title = "Endearing character"
@@ -32,6 +35,28 @@ const Music = () => {
             color = "#7abfe9"
           />
         </div>
+
+        <h1>BASSLINES</h1>
+        <p>
+          These are a bunch of shorter and simpler tracks
+          that I made based on different basslines 
+        </p>
+        <div className = "music-sectionh">
+          <MusicPlayerH 
+            title = "Endearing character"
+            cover = {endecharCover}
+            track = {endercharTrack}
+            color = "#bc95f0"
+          />
+          <MusicPlayerH
+            title = "Cute character"
+            cover = {cutecharCover}
+            track = {cutecharTrack}
+            color = "#7abfe9"
+          />
+        </div>
+        
+
       </>
     );
   };
